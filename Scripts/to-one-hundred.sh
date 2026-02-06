004df1e40b110d8bb2f101257caeae07dff9ed7a
nvegas1230 <vboxuser@TheNevadaVM.myguest.virtualbox.org>
2026-02-06 10:08:41 -0600

    Created the 'to-one-hundred.sh' script

--- Scripts/to-one-hundred.sh
+++ Scripts/to-one-hundred.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 number=$((($RANDOM%100)+1))
-guessed=false
 
-while !$guessed; do
+while true; do
 	read -p "Guess a number between 1 and 100: " guess
 	if [[ $guess -eq $number ]]; then
-		echo "You guessed the number! It was $number."
+		echo "You guessed the number! It was $number"
+		break
+	elif [[ $guess -lt $number ]]; then
+		echo "Your guess was too low"
+	else
+		echo "Your guess was too high"
 	fi
 done
