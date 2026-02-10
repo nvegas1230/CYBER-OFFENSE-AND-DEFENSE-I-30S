7554f23c1eaaf5df6c2fbbb0a1a1df6888225bfd
nvegas1230 <vboxuser@TheNevadaVM.myguest.virtualbox.org>
2026-02-10 10:37:52 -0600

    Made a simple sorting algorithm for script, appearently called bubble sort

--- Scripts/sort-numbers.sh
+++ Scripts/sort-numbers.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
 
+# USING BUBBLE SORT
+declare -a numberList
+mainLoop=true
+
+read -p "Input 10 numbers (seperated by spaces): " numbers
+for number in $numbers; do
+	numberList+=($number)
+done
+
+listLength=${#numberList[@]}
+
+while $mainLoop; do
+	declare -i passCount
+	passCount=0
+
+	for index in ${!numberList[@]}; do
+		num1=${numberList[index]}
+		num2=${numberList[index+1]}
+
+		if [[ $((index+1)) -eq $listLength ]]; then
+			passCount=$((passCount+1))
+			continue
+		fi
+
+		bool=false
+		if [[ $num1 -gt $num2 ]]; then
+			bool=true
+		fi
+
+		if $bool; then
+			numberList[index]=${num2}
+			numberList[index+1]=${num1}
+		else
+			passCount=$((passCount+1))
+		fi
+
+		# To see the sort in action
+		#echo "$passCount, ${numberList[@]}"
+	done
+
+	if [[ $passCount -eq $listLength ]]; then
+		mainLoop=false
+	fi
+done
+
+echo "Sorted list: ${numberList[@]}"
