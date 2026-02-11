d0729c5bed64509fec4413211c6967283c48abf5
nvegas1230 <vboxuser@TheNevadaVM.myguest.virtualbox.org>
2026-02-11 10:30:33 -0600

    did stuff with sorting numbers script, cant remember that well

--- Scripts/sort-numbers.sh
+++ Scripts/sort-numbers.sh
@@ -5,44 +5,62 @@ declare -a numberList
 mainLoop=true
 
 read -p "Input 10 numbers (seperated by spaces): " numbers
+read -p "Input sort method ( bubble / selection ): " sortMethod
+echo
+
 for number in $numbers; do
 	numberList+=($number)
 done
 
 listLength=${#numberList[@]}
+declare -i totalSwaps
+totalSwaps=0
 
 while $mainLoop; do
 	declare -i passCount
 	passCount=0
 
-	for index in ${!numberList[@]}; do
-		num1=${numberList[index]}
-		num2=${numberList[index+1]}
+	if [[ "$sortMethod" == "bubble" ]]; then
+		# USING BUBBLE SORT
+		# Bubble sort checks the current number and the number after it; if the current number is higher, it swaps them.
+		# This is repeated until a run through of the list happens without swapping any numbers
+		for index in ${!numberList[@]}; do
+			num1=${numberList[index]}
+			num2=${numberList[index+1]}
 
-		if [[ $((index+1)) -eq $listLength ]]; then
-			passCount=$((passCount+1))
-			continue
+			if [[ $((index+1)) -eq $listLength ]]; then
+				passCount=$((passCount+1))
+				continue
+			fi
+
+			bool=false
+			if [[ $num1 -gt $num2 ]]; then
+				bool=true
+			fi
+
+			if $bool; then
+				numberList[index]=${num2}
+				numberList[index+1]=${num1}
+				echo "Number $num1 at index $index swapped with number $num2 at index $((index+1))"
+				totalSwaps=$((totalSwaps+1))
+			else
+				passCount=$((passCount+1))
+			fi
+
+			# To see the sort in action
+			#echo "$passCount, ${numberList[@]}"
+		done
+
+		if [[ $passCount -eq $listLength ]]; then
+			mainLoop=false
 		fi
-
-		bool=false
-		if [[ $num1 -gt $num2 ]]; then
-			bool=true
-		fi
-
-		if $bool; then
-			numberList[index]=${num2}
-			numberList[index+1]=${num1}
-		else
-			passCount=$((passCount+1))
-		fi
-
-		# To see the sort in action
-		#echo "$passCount, ${numberList[@]}"
-	done
-
-	if [[ $passCount -eq $listLength ]]; then
-		mainLoop=false
+	elif [[ "$sortMethod" == "selection" ]]; then
+		# USING SELECTION SORT
+		# Selection sort checks the current number and finds the lowest number in the list; if the current number is higher, it swaps them.
+		# This is repeated until a run through of the list happens without swapping any numbers
 	fi
 done
 
+echo
 echo "Sorted list: ${numberList[@]}"
+echo "Total swaps made: $totalSwaps"
